5db071abeb28d0dec872751f4c75646e2e31d1e8
Samuel Kniffin <99624366+kniffin11@users.noreply.github.com>
2022-08-09 11:26:13 -0400

    Delete SpringToolSuiteProjects directory

--- SpringToolSuiteProjects/CaresoftInterfaces/src/com/caresoft/clinicapp/HIPAACompliantUser.java
+++
@@ -1,6 +0,0 @@
-package com.caresoft.clinicapp;
-
-public interface HIPAACompliantUser {
-	abstract boolean accessAuthorized(Integer confirmedAuthID);
-	abstract boolean assignPin(int pin);
-}
--- SpringToolSuiteProjects/ZooKeeper/src/models/MammalsTest.java
+++
@@ -1,9 +0,0 @@
-package models;
-
-public class MammalsTest {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
